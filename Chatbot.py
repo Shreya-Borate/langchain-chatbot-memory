a69cbd71696bff4eccd6fbd2728f868e53276d61
Shreya-Borate <borateshreya43@gmail.com>
2026-09-14 23:00:44 +0530

    Added memory

--- Chatbot.py
+++ Chatbot.py
@@ -7,10 +7,15 @@ model = ChatGoogleGenerativeAI(
     model="gemini-3.6-flash"
 )
 
+chat_history = []
+
 while True :
     user_input = input('You: ')
+    chat_history.append(user_input)
     if user_input == 'exit':
         break
-    result = model.invoke(user_input)
-    print("AI: ",result.content)
+    result = model.invoke(chat_history)
+    chat_history.append(result.content[0]["text"])
+    print("AI: ",result.content[0]["text"])
+print(chat_history)
      
