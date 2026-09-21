cd35fcae29a24725fbc7c3b6a38fda6066d0b271
Shreya-Borate <borateshreya43@gmail.com>
2026-09-21 11:31:11 +0530

    Add system message to chatbot

--- Chatbot.py
+++ Chatbot.py
@@ -1,4 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,15 +8,17 @@ model = ChatGoogleGenerativeAI(
     model="gemini-3.6-flash"
 )
 
-chat_history = []
+chat_history = [
+    SystemMessage(content='You are a helpful AI assistant')
+]
 
 while True :
     user_input = input('You: ')
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input == 'exit':
         break
     result = model.invoke(chat_history)
-    chat_history.append(result.content[0]["text"])
+    chat_history.append(AIMessage(content=(result.content[0]["text"])))
     print("AI: ",result.content[0]["text"])
 print(chat_history)
      
